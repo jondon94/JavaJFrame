90390cfa315099e64023c510cff206d947160962
jondon94 <jondonaldson94@gmail.com>
2018-03-22 21:40:37 +0000

    JFrame tutorial

---
+++ JFrameDemo.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class JFrameDemo {
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("JFrameDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel emptyLabel = new JLabel("");
+        emptyLabel.setPreferredSize(new Dimension(175, 100));
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
