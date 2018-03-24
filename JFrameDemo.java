062013cc51f31371d024d581a1488775c9cd9935
jondon94 <jondonaldson94@gmail.com>
2018-03-24 13:48:15 +0000

    minor adjustments

--- JFrameDemo.java
+++ JFrameDemo.java
@@ -1,3 +1,5 @@
+//javac JFrameDemo.java
+//java JFrameDemo
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -6,11 +8,13 @@ public class JFrameDemo {
     private static void createAndShowGUI() {
         JFrame frame = new JFrame("JFrameDemo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JLabel emptyLabel = new JLabel("");
-        emptyLabel.setPreferredSize(new Dimension(175, 100));
+        JLabel emptyLabel = new JLabel("hello world");
+        emptyLabel.setPreferredSize(new Dimension(325, 250));
         frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
         frame.pack();
         frame.setVisible(true);
+
+        JPanel contentpane = new JPanel(new BorderLayout());
     }
 
     public static void main(String[] args) {
